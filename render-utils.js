a8119224fd254daf34285a51c1cef332ca254c25
benni maxon <benni.maxon@gmail.com>
2022-12-09 17:35:51 -0800

    built prompt entry render function

--- render-utils.js
+++ render-utils.js
@@ -18,6 +18,8 @@ export function renderClientSetupUI() {
     submitButton.textContent = 'Go!';
     usernameInput.id = 'username-input';
     roomCodeInput.id = 'roomcode-input';
+    usernameInput.type = 'text';
+    roomCodeInput.type = 'text';
 
     // apply css classes
     uiBox.classList.add('ui-box-center');
@@ -32,4 +34,25 @@ export function renderClientSetupUI() {
     uiBox.append(usernameLabel, roomCodeLabel, submitButton);
 
     return uiBox;
+}
+
+export function renderPromptEntryUI() {
+    // create html elements
+    const uiBox = document.createElement('div');
+    const promptLabel = document.createElement('label');
+    const promptInput = document.createElement('input');
+    const submitButton = document.createElement('button');
+    // set properties of html elements
+    promptLabel.textContent = 'Prompt: ';
+    submitButton.textContent = 'Submit';
+    promptInput.id = 'prompt-input';
+    promptInput.type = 'text';
+    // apply css classes
+    uiBox.classList.add('ui-box-center');
+    uiBox.classList.add('flexbox-column-centered');
+    // event handler for clicking submit button
+    submitButton.addEventHandler();
+    // append all the stuff to other stuff and then to the div we're returning
+    promptLabel.append(promptInput);
+    uiBox.append(promptLabel, submitButton);
 }
