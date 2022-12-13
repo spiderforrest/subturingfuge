0bacc0751c79f8cd16a1ff2f671c758f63142d01
benni maxon <benni.maxon@gmail.com>
2022-12-13 12:14:34 -0800

    configured sign-in/sign-out link on main menu

--- app.js
+++ app.js
@@ -1,16 +1,21 @@
 /* Imports */
-// this will check if we have a user and set signout link if it exists
-import './auth/user.js';
-import { getUser } from './fetch-utils.js';
+import { getUser, signOutUser } from './fetch-utils.js';
 
 /* Get DOM Elements */
 const emailEl = document.querySelector('#logged-in-user-email');
+const signInOutEl = document.querySelector('#sign-out-link');
 
 /* State */
 
 /* Events */
 self.addEventListener('load', () => {
     emailEl.textContent = `[${getUser().email} ~] $`;
+});
+
+signInOutEl.textContent = getUser() ? 'Sign Out' : 'Sign In';
+signInOutEl.href = getUser() ? '/' : 'auth';
+signInOutEl.addEventListener('click', async () => {
+    await signOutUser();
 });
 
 /* Display Functions */
